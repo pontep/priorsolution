cbe72ab7eb976f5cdd56e8cf05b82f0d5a9edeb5
pontep <pontep.taveeshup@gmail.com>
2020-10-05 00:28:43 +0700

    decorating admin index

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <v-container grid-list-xs>
-    <div class="text-center">
-      <h1>Admin route</h1>
-      <p>AAAAAAAAAAAAAAA</p>
-      <v-img src="/question.jpg" max-height="500" max-width="250"></v-img>
-    </div>
+  <v-container>
+    <v-card outlined class="mx-auto" max-width="400">
+      <v-card-title primary>
+        <v-icon>mdi-account</v-icon>
+        <span>นายพรเทพ ทวีทรัพย์</span>
+      </v-card-title>
+      <v-divider class=""></v-divider>
+      <v-card-text>
+        <div class="ma-2">
+          <v-row no-gutters>
+            <span>
+              <v-icon>mdi-school</v-icon> มหาวิทยาลัยเทคโนโลยีสุรนารี</span
+            >
+          </v-row>
+          <v-row no-gutters>
+            <span>
+              <v-icon>mdi-account-details</v-icon> รหัสนักศึกษา B6000783</span
+            >
+          </v-row>
+
+          <v-img
+            class="my-2 grey lighten-3"
+            src="/question.jpg"
+            contain
+            alt="Pontep Mission"
+          ></v-img>
+        </div>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
