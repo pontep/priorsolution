6ddcbbfa7f2bf21c0e495f03ecb4f27691b63d5c
pontep <pontep.taveeshup@gmail.com>
2020-10-07 22:09:57 +0700

    hide console

--- store/index.js
+++ store/index.js
@@ -4,12 +4,12 @@ import Cookie from 'js-cookie'
 
 export const actions = {
   nuxtClientInit({ commit }) {
-    console.log(' nuxtClientInit work')
+    // console.log(' nuxtClientInit work')
     // if (process.server && process.static) return
     // if (!req.headers.cookie) return
     const cookie = Cookie.get('access_token')
-    console.log('cookie')
-    console.log(cookie)
+    // console.log('cookie')
+    // console.log(cookie)
     // const parsed = cookieparser.parse(cookie)
     // console.log('parsed')
     // console.log(parsed)
@@ -17,11 +17,11 @@ export const actions = {
 
     if (!accessTokenCookie) return
 
-    console.log('accessTokenCookie')
-    console.log(accessTokenCookie)
+    // console.log('accessTokenCookie')
+    // console.log(accessTokenCookie)
     const decoded = JWTDecode(accessTokenCookie)
     if (decoded) {
-      console.log('WORKSSS')
+      // console.log('WORKSSS')
       commit('users/SET_USER', {
         uid: decoded.user_id,
         email: decoded.email,
